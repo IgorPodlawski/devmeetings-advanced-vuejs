52ad63c71028049c9a4bd52f6cd43e261cd78218
Igor Podlawski <igor.podlawski@gmail.com>
2019-03-16 15:15:44 +0100

    [#4.4] Real one 4.4 – add settings module

---
+++ src/modules/settings.module.js
@@ -0,0 +1,13 @@
+const initialState = () => ({
+  prop1: '',
+});
+
+const state = initialState;
+
+export default {
+  namespaced: true,
+  // getters,
+  // actions,
+  // mutations,
+  state,
+};
